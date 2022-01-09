b72494f91bff7f205509d64cf3e383d147674740
Darius Mejeras <darius.mejeras@gmail.com>
2022-01-09 16:52:05 +0200

    remove dependencies

---
+++ render/helpers.go
@@ -0,0 +1,63 @@
+package render
+
+import (
+	"bytes"
+	"encoding/json"
+	"go/doc"
+	"html/template"
+	"strings"
+
+	"github.com/fatih/structtag"
+	"github.com/markbates/inflect"
+	"github.com/pkg/errors"
+)
+
+var defaultRuleset = inflect.NewDefaultRuleset()
+
+func toJSONHelper(v interface{}) (template.HTML, error) {
+	b, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		return "", err
+	}
+	return template.HTML(b), nil
+}
+
+func formatCommentLine(s string) template.HTML {
+	var buf bytes.Buffer
+	doc.ToText(&buf, s, "", "", 2000)
+	s = strings.TrimSpace(buf.String())
+	return template.HTML(s)
+}
+
+func formatCommentText(s string) template.HTML {
+	var buf bytes.Buffer
+	doc.ToText(&buf, s, "// ", "", 80)
+	return template.HTML(buf.String())
+}
+
+func formatCommentHTML(s string) template.HTML {
+	var buf bytes.Buffer
+	doc.ToHTML(&buf, s, nil)
+	return template.HTML(buf.String())
+}
+
+// formatTags formats a list of struct tag strings into one.
+// Will return an error if any of the tag strings are invalid.
+func formatTags(tags ...string) (template.HTML, error) {
+	alltags := &structtag.Tags{}
+	for _, tag := range tags {
+		theseTags, err := structtag.Parse(tag)
+		if err != nil {
+			return "", errors.Wrapf(err, "parse tags: `%s`", tag)
+		}
+		for _, t := range theseTags.Tags() {
+			alltags.Set(t)
+		}
+	}
+	tagsStr := alltags.String()
+	if tagsStr == "" {
+		return "", nil
+	}
+	tagsStr = "`" + tagsStr + "`"
+	return template.HTML(tagsStr), nil
+}
--- render/render.go
+++ render/render.go
@@ -1,84 +1,32 @@
 package render
 
 import (
-	"bytes"
-	"encoding/json"
-	"go/doc"
-	"html/template"
-	"strings"
-
 	"github.com/damejeras/gorpc/parser"
-	"github.com/fatih/structtag"
-	"github.com/gobuffalo/plush"
-	"github.com/markbates/inflect"
-	"github.com/pkg/errors"
+	"io"
+	"text/template"
 )
 
-var defaultRuleset = inflect.NewDefaultRuleset()
+type definition struct {
+	parser.Definition
+	Params map[string]interface{}
+}
 
-// Render renders the template using the Definition.
-func Render(template string, def parser.Definition, params map[string]interface{}) (string, error) {
-	ctx := plush.NewContext()
-	ctx.Set("camelize_down", camelizeDown)
-	ctx.Set("camelize_up", camelizeUp)
-	ctx.Set("def", def)
-	ctx.Set("params", params)
-	ctx.Set("json", toJSONHelper)
-	ctx.Set("format_comment_line", formatCommentLine)
-	ctx.Set("format_comment_text", formatCommentText)
-	ctx.Set("format_comment_html", formatCommentHTML)
-	ctx.Set("format_tags", formatTags)
-	s, err := plush.Render(template, ctx)
+func Render(t string, out io.Writer, def parser.Definition, params map[string]interface{}) error {
+	tpl, err := template.New("test").Funcs(map[string]interface{}{
+		"camelize_down":       camelizeDown,
+		"camelize_up":         camelizeUp,
+		"json":                toJSONHelper,
+		"format_comment_line": formatCommentLine,
+		"format_comment_text": formatCommentText,
+		"format_comment_html": formatCommentHTML,
+		"format_tags":         formatTags,
+	}).Parse(t)
 	if err != nil {
-		return "", err
+		return err
 	}
-	return s, nil
-}
 
-func toJSONHelper(v interface{}) (template.HTML, error) {
-	b, err := json.MarshalIndent(v, "", "\t")
-	if err != nil {
-		return "", err
-	}
-	return template.HTML(b), nil
-}
-
-func formatCommentLine(s string) template.HTML {
-	var buf bytes.Buffer
-	doc.ToText(&buf, s, "", "", 2000)
-	s = strings.TrimSpace(buf.String())
-	return template.HTML(s)
-}
-
-func formatCommentText(s string) template.HTML {
-	var buf bytes.Buffer
-	doc.ToText(&buf, s, "// ", "", 80)
-	return template.HTML(buf.String())
-}
-
-func formatCommentHTML(s string) template.HTML {
-	var buf bytes.Buffer
-	doc.ToHTML(&buf, s, nil)
-	return template.HTML(buf.String())
-}
-
-// formatTags formats a list of struct tag strings into one.
-// Will return an error if any of the tag strings are invalid.
-func formatTags(tags ...string) (template.HTML, error) {
-	alltags := &structtag.Tags{}
-	for _, tag := range tags {
-		theseTags, err := structtag.Parse(tag)
-		if err != nil {
-			return "", errors.Wrapf(err, "parse tags: `%s`", tag)
-		}
-		for _, t := range theseTags.Tags() {
-			alltags.Set(t)
-		}
-	}
-	tagsStr := alltags.String()
-	if tagsStr == "" {
-		return "", nil
-	}
-	tagsStr = "`" + tagsStr + "`"
-	return template.HTML(tagsStr), nil
+	return tpl.Execute(out, definition{
+		Definition: def,
+		Params:     params,
+	})
 }
